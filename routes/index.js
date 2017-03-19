608669384dcedd2df7409287baa24f5a9e0094e1
HiroAgustin <hiroagustin@gmail.com>
2017-03-19 18:25:15 -0300

    Add some styles. Questions from server. Create mixins.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,28 @@ const router = express.Router();
 
 /* GET home page. */
 router.get('/', (req, res, next) =>
-  res.render('index', { title: 'NerdQuiz' })
+  res.render('index', {
+    questions: [
+      {
+        predicate: 'You had a holiday romance and your next step will be…',
+        answers: [
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+          'Duis aute irure dolor in reprehenderit in voluptate.',
+          'Excepteur sint occaecat cupidatat non proident.'
+        ]
+      },
+      {
+        predicate: 'You had a holiday romance and your next step will be…',
+        answers: [
+          'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+          'Ut enim ad minim veniam, quis nostrud exercitation ullamco.',
+          'Duis aute irure dolor in reprehenderit in voluptate.',
+          'Excepteur sint occaecat cupidatat non proident.'
+        ]
+      }
+    ]
+  })
 );
 
 module.exports = router;
