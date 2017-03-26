e1df0f05cd5fae2fc03a6cf76145dc8fc87d34ca
HiroAgustin <hiroagustin@gmail.com>
2017-03-26 18:25:55 -0300

    Update answer objects to add flexibitliy ;

--- routes/index.js
+++ routes/index.js
@@ -7,43 +7,253 @@ router.get('/', (req, res, next) =>
     questions: [
       {
         predicate: 'Plan de viernes de noche…',
-        answers: ['Salir', 'Cine o TV', 'Videojuegos', 'Ver animés']
+        answers: [
+          {
+            label: 'Salir',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Cine o TV',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Videojuegos',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Ver animés',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'No podés vivir sin…',
-        answers: ['Laptop', 'Celular', 'Heladera', 'Compu de escritorio con dos monitores']
+        answers: [
+          {
+            label: 'Laptop',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Celular',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Heladera',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Compu de escritorio con dos monitores',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Sabés…',
-        answers: ['Inglés o Francés', 'Klingon, élfico o dothraki', 'C++', 'Ninguno']
+        answers: [
+          {
+            label: 'Inglés o Francés',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Klingon, élfico o dothraki',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'C++',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Ninguno',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Leés…',
-        answers: ['Poco y nada', 'Uno de autoayuda', 'Novelas todo el tiempo', 'Comics, el Hobbit y El Silmarillion']
+        answers: [
+          {
+            label: 'Poco y nada',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Uno de autoayuda',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Novelas todo el tiempo',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Comics, el Hobbit y El Silmarillion',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Tenés cubo mágico…',
-        answers: ['No', 'Sí, pero nunca pude armarlo', 'Lo armé con un tutorial', '¿Cuántas veces querés que lo arme y desarme?']
+        answers: [
+          {
+            label: 'No',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Sí, pero nunca pude armarlo',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Lo armé con un tutorial',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: '¿Cuántas veces querés que lo arme y desarme?',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Tus amistades te suelen pedir…',
-        answers: ['Plata', 'Ropa', 'Que les arregle la compu', 'Debatir sobre Android o iOS']
+        answers: [
+          {
+            label: 'Plata',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Ropa',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Que les arregle la compu',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Debatir sobre Android o iOS',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Deportes…',
-        answers: ['Fútbol', 'Basket', 'Esgrima', 'Ajedrez']
+        answers: [
+          {
+            label: 'Fútbol',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Basket',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Esgrima',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Ajedrez',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Jugás a…',
-        answers: ['FIFA', 'Candy Crush', 'World of Warcraft', 'Juegos de rol']
+        answers: [
+          {
+            label: 'FIFA',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Candy Crush',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'World of Warcraft',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Juegos de rol',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Tu definición de CRACK es…',
-        answers: ['El Lucho', 'Lio', 'Neil DeGrass Tyson', 'Wozniak']
+        answers: [
+          {
+            label: 'El Lucho',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Lio',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Neil DeGrass Tyson',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Wozniak',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       },
       {
         predicate: 'Para una ocasión especial te vestís…',
-        answers: ['Casual, sencillito', 'Lo que es moda, no incomoda', 'Me dicen fashion victim', 'Con un cosplay']
+        answers: [
+          {
+            label: 'Casual, sencillito',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Lo que es moda, no incomoda',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Me dicen fashion victim',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          },
+          {
+            label: 'Con un cosplay',
+            value: 0,
+            img: '/images/sample-1.jpg'
+          }
+        ]
       }
     ]
   })
